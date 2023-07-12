b44af121187ecb10841fc9f3ff756bb9d403de2d
Dener <mundoem3di@gmail.com>
2023-07-12 00:41:14 +0000

    change style

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 import teste from "./src/components/quadrado.mjs"
 
+var checkbox = document.querySelector('#switch');
 
+const changeThemeLg = () => {
+    document.documentElement.setAttribute("ui-theme", "light");
+}
 
-console.log(teste())
+const changeThemeDk = () => {
+    document.documentElement.setAttribute("ui-theme", "dark");
+}
+
+// checkbox.addEventListener('change', function() {
+//     if(this.checked) {
+
+//         document.documentElement.setAttribute('ui-theme', 'dark')
+//     } else {
+
+//         document.documentElement.setAttribute('ui-theme', 'light')
+//     }
+// })
+
+// const btnChangeTheme = document.querySelector("#change-theme");
+
+// const changeThemeLight = () => {
+//   document.documentElement.setAttribute("ui-theme", "light");
+//   localStorage.setItem("ui-theme", "light");
+// };
+
+// const changeThemeDark = () => {
+//   document.documentElement.setAttribute("ui-theme", "dark");
+//   localStorage.setItem("ui-theme", "dark");
+// };
+
+// if (localStorage.getItem("ui-theme") == "dark") {
+//   btnChangeTheme.checked = true;
+//   changeThemeDark();
+// }
+
+// btnChangeTheme.addEventListener("change", () => {
+//   if (!btnChangeTheme.checked) {
+//     changeThemeLight();
+//   } else {
+//     changeThemeDark();
+//   }
+// });
