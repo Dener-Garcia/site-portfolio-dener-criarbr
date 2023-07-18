e9c359541667e4000f28710eca39eac30ca99441
Dener <mundoem3di@gmail.com>
2023-07-18 03:45:28 +0000

    style: inputs radio finish

--- src/functions/acessibility-menu/acessibility-meu.mjs
+++ src/functions/acessibility-menu/acessibility-meu.mjs
@@ -3,27 +3,17 @@ const acessibilityMenu = document.querySelector(".ui-options")
 
 
 const Acessibility = () =>{
-    showMenu.addEventListener('click', ()=>{
-        acessibilityMenu.classList.remove("d-none")
-        acessibilityMenu.classList.add("d-grid")
+    showMenu.addEventListener('click', (e)=>{
+        acessibilityMenu.classList.toggle("d-grid")
 
-        acessibilityMenu.classList.contains("d-grid")
-        console.log("tem a class")
+        document.onclick = function(e){
+            console.log(acessibilityMenu.contains(e.target),showMenu.contains(e.target))
+            if (!acessibilityMenu.contains(e.target) && !showMenu.contains(e.target)) {
+                acessibilityMenu.classList.remove("d-grid")
+            }
+        }
     })
 
-const closedMenu = document.addEventListener("click", (){
-    if (acessibilityMenu.classList.contains("d-grid")) {
-        acessibilityMenu.classList.remove("d-grid")
-    }
-})
-
-// const closedMenu = (e)=>{
-//     const menuClicked = acessibilityMenu.contains(e.target)
-//     if (!menuClicked) {
-//         acessibilityMenu.classList.remove("d-grid");
-//         acessibilityMenu.classList.add("d-none");
-//         document.removeEventListener("click", closedMenu);
-// }
 
 }
 
